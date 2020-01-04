faadbfb29b1864cb1e1e4c9b4e171a2edbc97060
Leon Jamison <leonjamison@icloud.com>
2020-01-04 23:38:09 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,9 +1,9 @@
-def plus_two(num)
-	num + 2
-	num
-	# binding.pry
-end
-Alternative solution
+# def plus_two(num)
+# 	num + 2
+# 	num
+# 	# binding.pry
+# end
+# Alternative solution
 def plus_two(num)
 	  num = num + 2
 	  num
